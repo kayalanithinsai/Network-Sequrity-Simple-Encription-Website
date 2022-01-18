5d2bc9981ee64c8899f5c8d08fb26cbd8d21dd9d
kayalanithinsai <nithinkayala@gmail.com>
2022-01-18 22:08:21 +0530

    2nd version backend

--- mysite/simple_enc_dec/views.py
+++ mysite/simple_enc_dec/views.py
@@ -4,6 +4,14 @@ from django.template import loader
 
 # Create your views here.
 def encrypt_func(encrypt_str):
+    """
+    :param encrypt_str:
+    :return encrypted string:
+    if the character is between A and Z or a and z we encrypt these characters else we leave other characters.
+    The ith alphabet from the beginning is mapped to ith character from the end.
+    we could use this function for both encryption and decryption purposes.
+    """
+
     encrypted_str = ''
     for c in encrypt_str:
         if 'A' <= c <= 'Z':
@@ -26,10 +34,10 @@ def first_view(request):
             encrypt_str = request.POST.get('encrypt_str')
             template = loader.get_template('simple_enc_dec/index.html')
             encrypted_str = encrypt_func(encrypt_str)
-            return HttpResponse(template.render({'encrypt_str': '', 'decrypt_str': encrypted_str}, request))
+            return HttpResponse(template.render({'encrypt_str': encrypt_str, 'decrypt_str': encrypted_str}, request))
         else:
-            decrypt_str = request.POST.get('decrypt_str')
+            decrypt_str = request.POST.get('encrypt_str')
             template = loader.get_template('simple_enc_dec/index.html')
             decrypted_str = encrypt_func(decrypt_str)
-            return HttpResponse(template.render({'encrypt_str': decrypted_str, 'decrypt_str': ''}, request))
+            return HttpResponse(template.render({'encrypt_str': decrypt_str, 'decrypt_str': decrypted_str}, request))
 
